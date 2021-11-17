a31951d688ff9fe875df15e1f5497a817f0b992c
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-17 21:02:34 +0100

    feat: tailwind config

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
   theme: {
-    extend: {},
+    fontFamily: {
+      bitter: ["Bitter", "serif", "system-ui"],
+      roboto: ["Roboto", "serif", "system-ui"],
+    },
+    extend: {
+      colors: {
+        gray: "#373838",
+        coolGray: colors.coolGray,
+        gray3: "#828282",
+      },
+    },
   },
   variants: {
     extend: {},
