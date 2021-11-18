2370ca8db4d2f0067a0272e19b5b0bb722e9277d
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-18 12:45:28 +0100

    feat: time record model

---
+++ packages/server/src/models/index.ts
@@ -0,0 +1,1 @@
+export { TimeRecord } from "./time-record-model";
---
+++ packages/server/src/models/time-record-model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const timeRecordSchema = new Schema(
+  {
+    time: {
+      type: Number,
+      required: [true, "The time is the only required parameter"],
+    },
+  },
+  {
+    strict: true,
+    timestamps: true,
+  },
+);
+
+export const TimeRecord = mongoose.model("timeRecord", timeRecordSchema);
+
+// module.exports = Category;
