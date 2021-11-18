08d64cd248440bd3f8495bb6f8e4c928950797b7
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-18 13:37:38 +0100

    feat: frontend main functionality finished

--- packages/client/src/components/MainTimer/MainTimer.tsx
+++ packages/client/src/components/MainTimer/MainTimer.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTotalTime } from "../../api/timer-api";
+import { secondsToTime } from "../../utils/time-utils";
 import TimerButton from "../TimerButton";
 
 export default function MainTimer() {
+  const [totalTime, setTotalTime] = useState({ loaded: false, time: 0 });
+
+  const fetchTotalTime = async () => {
+    try {
+      setTotalTime((prevState) => ({ ...prevState, loaded: false }));
+      const {
+        data: { data },
+      }: { data: { data: { total: number; totalLogs: number } } } =
+        await getTotalTime();
+      setTotalTime({ loaded: true, time: Number(data.total) });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTotalTime();
+  }, []);
   return (
-    <div className="flex flex-col gap-y-4 mx-auto">
-      <p className="text-6xl">00:00:00</p>
+    <div className="flex flex-col gap-y-8 mx-auto">
+      <p className="text-6xl">{secondsToTime(totalTime.time)}</p>
       <div className="">
-        <TimerButton />
+        <TimerButton setTotalTime={setTotalTime} />
       </div>
     </div>
   );
--- packages/client/src/components/TimerButton/TimerButton.tsx
+++ packages/client/src/components/TimerButton/TimerButton.tsx
@@ -1,15 +1,67 @@
-import React from "react";
-// import {IoPlayOutline, IoPauseOutline} from "react-icons/io";
-import { IoPlayOutline } from "react-icons/io5";
+import React, { useState, useEffect } from "react";
+import { IoPlayOutline, IoPauseOutline } from "react-icons/io5";
+import { addTime } from "../../api/timer-api";
+import { secondsToTime } from "../../utils/time-utils";
+// import { IoPlayOutline } from "react-icons/io5";
 
-export default function TimerButton() {
+export default function TimerButton({
+  setTotalTime,
+}: {
+  setTotalTime: React.Dispatch<
+    React.SetStateAction<{
+      loaded: boolean;
+      time: number;
+    }>
+  >;
+}) {
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(0); // time is in seconds
+
+  const postTime = async () => {
+    try {
+      const { data }: { data: { message: string; total: number } } =
+        await addTime(time);
+      setTotalTime({ loaded: true, time: Number(data.total) });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const interval: NodeJS.Timeout = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+
+    if (!isActive) {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive]);
+
+  const handleClick = () => {
+    if (isActive) {
+      console.log(`Registered time is ${time} seconds`);
+      postTime();
+      setTime(0);
+    }
+
+    setIsActive(!isActive);
+  };
+
   return (
     <button
       type="button"
       className="h-10 w-52 flex flex-row justify-center align-middle gap-x-5 bg-gray dark:bg-white text-white dark:text-gray text-sm mx-auto transition-colors duration-300"
+      onClick={handleClick}
     >
-      <IoPlayOutline size="18px" className="inline my-auto" />
-      <span className="my-auto leading-none">0:00:00</span>
+      {isActive ? (
+        <IoPauseOutline size="18px" className="inline my-auto" />
+      ) : (
+        <IoPlayOutline size="18px" className="inline my-auto" />
+      )}
+      <span className="my-auto leading-none">{secondsToTime(time)}</span>
     </button>
   );
 }
