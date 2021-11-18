db138b8ade81b6c445163a347ec1a0cf8631afa3
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-18 19:03:03 +0100

    feat: added CORS config for API

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { config } from "./config";
 import { timeRecordRouter } from "./routes";
 
 export const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+const options: cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,POST",
+  origin: config.client.url,
+  preflightContinue: false,
+};
+
+app.use(cors(options));
 
 app.use("/api/time", timeRecordRouter);
